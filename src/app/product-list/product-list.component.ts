77f3671ab1128ea021088d4f04023e37ef3784fa
neelima230123 <123529769+neelima230123@users.noreply.github.com>
2023-02-13 09:43:14 +0530

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 import { products } from '../products';
 import { customersData } from '../customersData';
 
--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.css']
 })
-export class ProjectDetailsComponent {
+export class ProjectDetailsComponent implements OnInit {
 
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+     const routeParams = this.route.snapshot.paramMap;
+     const productIdFromRoute = Number(routeParams.get('productId'));
+   this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+
+  
+  
 }
+
+
